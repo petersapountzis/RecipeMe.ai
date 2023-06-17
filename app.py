b6b53947146a377c34166d3664034341791ad459
petersapountzis <peter.sapountzis@gmail.com>
2023-06-17 11:23:08 -0700

    form data collection

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import openai
-import API_KEY_OPENAI
+from config import API_KEY_OPENAI  
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 
+
+
+
+def getGPTResponse(prompt):
+    openai.api_key = API_KEY_OPENAI
+    response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=200)
+    return response.choices[0].text.strip()
+
+
+@app.route('/', methods =["GET", "POST"])
+def getFormData():
+    protein = request.form.get("protein")
+    cals = request.form.get("calories")
+    ingredients = request.form.get("ingredients")
+    servings = request.form.get("servings")
+
+    return 'Success!'
+
+       
 
 
 if __name__ == '__main__':
