fb751cd02da27465fd24d67dcaa7bbe1e1130649
petersapountzis <peter.sapountzis@gmail.com>
2023-06-26 13:00:23 -0700

    remove config import

--- recipeMe/__init__.py
+++ recipeMe/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import openai
-from recipeMe.config import API_KEY_OPENAI  
+  
 import re
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
